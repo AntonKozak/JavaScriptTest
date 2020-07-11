cc98fff89e4e785426ce09cb2bb68a352a5539cf
Your Name <kozakanton@i.ua>
2020-07-11 09:46:24 +0200

    add1

---
+++ Electric Light Bulb/Bulb_on_off.js
@@ -0,0 +1,7 @@
+
+function switch_on() {
+    document.getElementById('myImage').src='img/pic_bulbon.gif'
+}
+function switch_off() {
+    document.getElementById('myImage').src='img/pic_bulboff.gif'
+}
